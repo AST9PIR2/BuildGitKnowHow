6feb1ba74491588b2078bbfa9e5665455df32052
AST9PIR2 <92747125+AST9PIR2@users.noreply.github.com>
2023-01-01 18:42:17 +0100

    I have now glue waht I am doing

---
+++ cmake-build-debug/CMakeFiles/BuildGitKnowHow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BuildGitKnowHow"
+  "BuildGitKnowHow.pdb"
+  "CMakeFiles/BuildGitKnowHow.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BuildGitKnowHow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BuildGitKnowHow.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BuildGitKnowHow.
+# This may be replaced when dependencies are built.
