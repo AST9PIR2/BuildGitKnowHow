47321a655837b36d4fcee4ce04d7235ac964462d
AST9PIR2 <92747125+AST9PIR2@users.noreply.github.com>
2023-01-01 20:36:10 +0100

    Changed some lines in main to create merge conflict

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Thank you" << std::endl;
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Thank you" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
